a45d47a0620cdf5e9e2cf8ad9ed82dbd8044b056
jerrycow <jerryowchiu@gmail.com>
2014-09-17 22:34:27 -0500

    blah

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+// open popup
+$('#button-1').click(function(){
+	$('#popup').fadeIn(2000);
+});
+
+// close popup
+$('.close-button').click(function(){
+	$('.popup').fadeOut(6000);
+});
+
+
+
+
+
+
